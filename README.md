0567dcecac44989b649458fd825506c0934249a4
Milan Martiniak <mino.martiniak@gmail.com>
2021-10-06 14:26:21 +0200

    Add parameters

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-# MMLib.Tools.Translator-
-.NET Global text translation tool 
+# MMLib.Tools.Translator
+
+`tr` is .NET Global text translation tool.
+
+## Installation
+`tr` is implemented as a .NET Core Global Tool, so its installation is quite simple:
+
+```bash
+dotnet tool install -g MMlib.Tools.Translator
+```
+
+To update it to the latest version, if it was installed previously, use:
+
+```bash
+dotnet tool update -g MMlib.Tools.Translator
+```
+
+## Usage
+
+```bash
+tr "Hello this is .NET Global text translation tool" -s en -t sk
+```
+
+`--source-language (-s)` and `--target-language (-t)` are optional. Default values are from user environment variables `MMLIB_TRANSLATOR_SOURCE_LNG` and `MMLIB_TRANSLATOR_TARGET_LNG`.
+
+## Set default languages
+
+Default languages can be set by `set-defaults` command.
+
+```bash
+tr set-defaults -s "en" -t "sk"
+```
+
+## Reverse
+
+Mostly a person needs to translate between two languages. If you have the default source and target language set and you want to do the translation just the other way around, you can use the `--reverse (-re)` parameter.
+
+```bash
+tr "Ahoj svet" -re
+```
+
+> @satano thanks for the idea.
+
+## Copy to clipboard
+
+If you need to copy the resulting translated text to the clipboard, you can use the `--copy-to-clipboard (-co)` parameter.
+
+```bash
+tr "Hello this is .NET Global text translation tool" --copy-to-clipboard
+```
--- src/MMlib.Tools.Translator/Program.cs
+++ src/MMlib.Tools.Translator/Program.cs
@@ -3,50 +3,140 @@ using System.CommandLine.Invocation;
 using System.Threading.Tasks;
 using Console = Colorful.Console;
 using System.Drawing;
+using System;
 
 namespace MMlib.Tools.Translator
 {
     class Program
     {
+        private const string _envPrefix = "MMLIB_TRANSLATOR_";
+        private const string _sourceLngKey = "SOURCE_LNG";
+        private const string _targetLngKey = "TARGET_LNG";
+
         static int Main(string[] args)
         {
             var rootCommand = new RootCommand
             {
                 new Option<string>(
                     new string[] {"--source-language" ,"-s"},
-                    () => "en",
-                    null),
+                    () => GetDefault(_sourceLngKey, "en"),
+                    "Source langulate"),
                 new Option<string>(
                     new string[] {"--target-language" ,"-t"},
-                    () => "sk",
-                    "Target language."),
+                    () => GetDefault(_targetLngKey, "sk"),
+                    "Target language"),
+                new Option<bool>(
+                    new string[] { "--reverse", "-re" },
+                    () => false,
+                    "Reverse source language with target language"),
+                new Option<bool>(
+                    new string[] { "--copy-to-clipboard", "-co" },
+                    () => false,
+                    "Copy result to clipboard")
             };
 
             rootCommand.AddArgument(new Argument("text"));
             rootCommand.Description = "Text translation tool";
+            rootCommand.AddCommand(CreateInfoCommand());
+            rootCommand.AddCommand(CreateSetDefaultLanguagesCommand());
 
 
-            rootCommand.Handler = CommandHandler.Create<string, string, string>(Translate);
+            rootCommand.Handler = CommandHandler.Create<string, string, string, bool, bool>(Translate);
 
             return rootCommand.InvokeAsync(args).Result;
         }
 
-        private static async Task Translate(string text, string sourceLanguage, string targetLanguage)
+        private static string GetDefault(string key, string defaultValue)
+            => Environment.GetEnvironmentVariable($"{_envPrefix}{key}", EnvironmentVariableTarget.User) ?? defaultValue;
+
+        private static void SetDefault(string key, string value)
+            => Environment.SetEnvironmentVariable($"{_envPrefix}{key}", value, EnvironmentVariableTarget.User);
+
+        private static async Task Translate(
+            string text,
+            string sourceLanguage,
+            string targetLanguage,
+            bool reverse,
+            bool copyToClipBoard)
         {
-            Console.WriteLine("Translating ...", Color.AliceBlue);
+            Console.WriteLine("Translating ...", Color.Yellow);
+
+            if (reverse)
+            {
+                var tmp = sourceLanguage;
+                sourceLanguage = targetLanguage;
+                targetLanguage = tmp;
+            }
+
             var translator = new GTranslate.Translators.Translator();
             var result = await translator.TranslateAsync(text, targetLanguage, sourceLanguage);
             ClearLine();
             Console.WriteLine(
-                $"Translated text from {result.SourceLanguage.Name} to {result.TargetLanguage.Name}:", Color.AliceBlue);
+                $"Translated text from {result.SourceLanguage.Name} to {result.TargetLanguage.Name}:", Color.Yellow);
             Console.WriteLine($"{result.Result}");
+
+            if (copyToClipBoard)
+            {
+                TextCopy.ClipboardService.SetText(result.Result);
+            }
         }
 
-        public static void ClearLine()
+        private static void ClearLine()
         {
             Console.SetCursorPosition(0, Console.CursorTop);
             Console.Write(new string(' ', Console.WindowWidth));
             Console.SetCursorPosition(0, Console.CursorTop - 1);
+        }
+
+        private static Command CreateSetDefaultLanguagesCommand()
+        {
+            var command = new Command("set-defaults") {
+                 new Option<string>(
+                    new string[] {"--source-language" ,"-s"},
+                    "Source langulate"),
+                new Option<string>(
+                    new string[] {"--target-language" ,"-t"},
+                    "Target language")
+            };
+            command.Handler = CommandHandler.Create<string, string>((sourceLanguage, targetLanguage) =>
+            {
+                SetDefault(_sourceLngKey, sourceLanguage);
+                SetDefault(_targetLngKey, targetLanguage);
+                Console.WriteLine("Defaults was set", Color.Yellow);
+                Console.WriteLine($"Source language: {sourceLanguage}", Color.Yellow);
+                Console.WriteLine($"Target language: {targetLanguage}", Color.Yellow);
+
+            });
+
+            return command;
+        }
+
+        private static Command CreateInfoCommand()
+        {
+            var command = new Command("info");
+            command.Handler = CommandHandler.Create(() =>
+            {
+                Console.WriteLine(@"
+  __  __  __  __  _       _  _     
+ |  \/  ||  \/  || |     (_)| |    
+ | \  / || \  / || |      _ | |__  
+ | |\/| || |\/| || |     | || '_ \ 
+ | |  | || |  | || |____ | || |_) |
+ |_|  |_||_|  |_||______||_||_.__/ 
+                                   
+                                   
+");
+                Console.WriteLine(@"
+ _____  ____  ____  _      ____  _     ____  _____  ____  ____ 
+/__ __\/  __\/  _ \/ \  /|/ ___\/ \   /  _ \/__ __\/  _ \/  __\
+  / \  |  \/|| / \|| |\ |||    \| |   | / \|  / \  | / \||  \/|
+  | |  |    /| |-||| | \||\___ || |_/\| |-||  | |  | \_/||    /
+  \_/  \_/\_\\_/ \|\_/  \|\____/\____/\_/ \|  \_/  \____/\_/\_\
+                                                               
+");
+            });
+
+            return command;
         }
     }
 }
